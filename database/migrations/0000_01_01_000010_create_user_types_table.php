b09f8af8d3876d3a01184fcf2975d43120b603fd
Admin <admin@AlanPacheco.local>
2024-04-10 15:38:32 -0700

    changed string for text

--- database/migrations/0000_01_01_000010_create_user_types_table.php
+++ database/migrations/0000_01_01_000010_create_user_types_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->id();
 
             $table->string('name');
-            $table->string('description')->nullable();
+            $table->text('description')->nullable();
 
             $table->timestamps();
         });
